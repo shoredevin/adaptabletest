a63296a2e97a98aae95d5ece684334be9abcf94e
shoredevin <shore.devin@gmail.com>
2022-09-12 13:36:55 -0400

    update

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -84,16 +84,16 @@ router.post("/dex", asyncMiddleware(async (req, res) => {
   //     'a': [ 'href' ]
   //   },
   // });
-  const { dexnum: dexnumIn, name, caught } = req.body;
+  const { dexnum, name, caught } = req.body;
   // const dexnum = req.body.dexnum;
   // const name = req.body.name;
   // const caught = req.body.caught;
-  const dexnum = sanitizeHtml(dexnumIn, {
-    allowedTags: [ 'a' ],
-    allowedAttributes: {
-      'a': [ 'href' ]
-    },
-  });
+  // const dexnum = sanitizeHtml(dexnumIn, {
+  //   allowedTags: [ 'a' ],
+  //   allowedAttributes: {
+  //     'a': [ 'href' ]
+  //   },
+  // });
   const result = await prisma.Pokedex.create({
     data: {
       dexnum,
