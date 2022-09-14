a02cd7ec3c77289ac3e8772472ded7d2996b4be7
shoredevin <shore.devin@gmail.com>
2022-09-14 08:20:00 -0400

    changing function

--- public/js/dex.js
+++ public/js/dex.js
@@ -30,22 +30,26 @@ async function yeet() {
 
 
 
+// async function json2table(data) {
+//     let myTable = document.querySelector("#myTable");
+//     myTable.style.display = "inline-block";
+//     let tbdy = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+//     tbdy.innerHTML = "";
+//     // let rows = data.response;
+//     let rows = data;
+//     // console.log(rows.length);
+//     const numCols = Object.keys(rows[1]).length;
+//     for (let i = 0; i < rows.length; i++) {
+//         let tr = document.createElement('tr');
+//         for(let k = 1; k < numCols; k++) {
+//             const val = Object.values(rows[i])[k];
+//             tr.appendChild(document.createElement('td'));
+//             tr.cells[k - 1].appendChild(document.createTextNode(val));
+//         }
+//         tbdy.appendChild(tr);
+//     }
+// }
+
 async function json2table(data) {
-    let myTable = document.querySelector("#myTable");
-    myTable.style.display = "inline-block";
-    let tbdy = document.getElementById('myTable').getElementsByTagName('tbody')[0];
-    tbdy.innerHTML = "";
-    // let rows = data.response;
-    let rows = data;
-    // console.log(rows.length);
-    const numCols = Object.keys(rows[1]).length;
-    for (let i = 0; i < rows.length; i++) {
-        let tr = document.createElement('tr');
-        for(let k = 1; k < numCols; k++) {
-            const val = Object.values(rows[i])[k];
-            tr.appendChild(document.createElement('td'));
-            tr.cells[k - 1].appendChild(document.createTextNode(val));
-        }
-        tbdy.appendChild(tr);
-    }
+    console.log('here');
 }
