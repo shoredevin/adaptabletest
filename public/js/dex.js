ce624bfc70c1151a071000411529d4d12497bb34
shoredevin <shore.devin@gmail.com>
2022-09-14 11:44:43 -0400

    typo

--- public/js/dex.js
+++ public/js/dex.js
@@ -36,7 +36,7 @@ async function yeet() {
 
 const json2table = ({ id, dexnum, name, caught }) => `
     <tr>
-        <td>${dexnum} <button class="delete-button" onclick="alert(${id})"><i class="fa-solid fa-trash-can"></i></button></td>
+        <td>${dexnum} <button class="delete-button" onclick="alert('${id}')"><i class="fa-solid fa-trash-can"></i></button></td>
         <td>${name}</td>
         <td>${caught}</td>
     </tr>
