88f3d33b42a17e2fed5f1fde55b68c0a0fd716c0
shoredevin <shore.devin@gmail.com>
2022-09-14 17:03:28 -0400

    log

--- public/js/dex.js
+++ public/js/dex.js
@@ -76,7 +76,7 @@ async function handleEdit(e) {
     // for(col of rows) { console.log(rows.contentEditable) };
     for(let i = 1; i < rows.length; i++) {
         // const bool = rows[i].contentEditable;
-        // console.log(rows[i].contentEditable)
+        console.log(rows[i].contentEditable)
         // if(bool == true) rows[i].contentEditable = false;
         // if(bool == false) rows[i].contentEditable = true;
         rows[i].contentEditable = true;
