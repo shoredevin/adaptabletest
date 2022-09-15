588447755fa08a2ae1bffab00b043976a1046fc0
shoredevin <shore.devin@gmail.com>
2022-09-15 12:12:27 -0400

    color

--- public/js/dex.js
+++ public/js/dex.js
@@ -78,8 +78,14 @@ async function handleEdit(e) {
     // for(col of rows) { console.log(rows.contentEditable) };
     for(let i = 1; i < rows.length; i++) {
         const bool = (rows[i].contentEditable === 'true');
-        if(bool) rows[i].contentEditable = false;
-        if(!bool) rows[i].contentEditable = true;
+        if(bool) {
+            rows[i].contentEditable = false;
+            e.style.color = "black";
+        }
+        if(!bool) {
+            rows[i].contentEditable = true;
+            e.style.color = "yellow";
+        }
         // rows[i].contentEditable = true;
     }   
 
