5e471c5d5fa3dfa1e96770af20bb64d23d976d41
shoredevin <36136055+shoredevin@users.noreply.github.com>
2022-09-15 18:56:55 -0400

    why do i do this

--- public/js/dex.js
+++ public/js/dex.js
@@ -17,8 +17,8 @@ const json2table = ({ id, dexnum, name, type1, type2, caught, shiny }) => `
         <td contenteditable="false">${ name }</td>
         <td contenteditable="false">${ type1 }</td>
         <td contenteditable="false">${ type2 }</td>
-        <td contenteditable="false">${ caught ? `<i class="fa-solid fa-star caught-button" onclick="patchJob(event, '${id}', { caught: false })"></i>` : `<i class="fa-regular fa-star caught-button" onclick="patchJob('${id}', { caught: true })"></i>` }</td>
-        <td contenteditable="false">${ shiny ? `<i class="fa-solid fa-heart shiny-button" onclick="patchJob(event, '${id}', { shiny: false })"></i>` : `<i class="fa-regular fa-heart shiny-button" onclick="patchJob('${id}', { shiny: true })"></i>` }</td>
+        <td contenteditable="false">${ caught ? `<i class="fa-solid fa-star caught-button" onclick="patchJob(event, '${id}', { caught: false })"></i>` : `<i class="fa-regular fa-star caught-button" onclick="patchJob(event, '${id}', { caught: true })"></i>` }</td>
+        <td contenteditable="false">${ shiny ? `<i class="fa-solid fa-heart shiny-button" onclick="patchJob(event, '${id}', { shiny: false })"></i>` : `<i class="fa-regular fa-heart shiny-button" onclick="patchJob(event, '${id}', { shiny: true })"></i>` }</td>
     </tr>
 `;
 
