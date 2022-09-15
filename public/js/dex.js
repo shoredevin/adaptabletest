7ea0d289b482e5d1a3466d9a5bec301215219c30
shoredevin <shore.devin@gmail.com>
2022-09-15 08:49:15 -0400

    idk at this point

--- public/js/dex.js
+++ public/js/dex.js
@@ -17,9 +17,9 @@ const json2table = ({ id, dexnum, name, caught }) =>
             </td>
             <td contenteditable="false">${ dexnum }</td>
             <td contenteditable="false">${ name }</td>
-            <td contenteditable="false">${ caught['true'] ? '<i class="fa-solid fa-star"></i>' : '<i class="fa-regular fa-star"></i>' }</td>
+            <td contenteditable="false">${ caught ? '<i class="fa-solid fa-star"></i>' : '<i class="fa-regular fa-star"></i>' }</td>
         </tr>
-    `
+    `;
 // };
     
 
