9a073f94f9faff0be5478a0768af0c1db0312e28
shoredevin <36136055+shoredevin@users.noreply.github.com>
2022-09-15 22:33:28 -0400

    old code

--- public/js/client.js
+++ public/js/client.js
@@ -1,110 +1,110 @@
-const apiFetch = async (url, method, body) => {
-    method = method || "GET";
-    const opts = { method };
-    if (body) {
-        opts.headers = {
-            "Content-Type": "application/json",
-        };
-        opts.body = JSON.stringify(body);
-    }
+// const apiFetch = async (url, method, body) => {
+//     method = method || "GET";
+//     const opts = { method };
+//     if (body) {
+//         opts.headers = {
+//             "Content-Type": "application/json",
+//         };
+//         opts.body = JSON.stringify(body);
+//     }
 
-    const resp = await fetch(url, opts);
-    if (!resp.ok) {
-        const text = await resp.text();
-        throw new Error(`Error from API: ${resp.statusText}: ${text}`);
-    }
-    return resp;
-};
+//     const resp = await fetch(url, opts);
+//     if (!resp.ok) {
+//         const text = await resp.text();
+//         throw new Error(`Error from API: ${resp.statusText}: ${text}`);
+//     }
+//     return resp;
+// };
 
-const initTodo = () => {
-    // INSERT handleTodoDelete HERE
-    const handleTodoDelete = (ev, id) => {
-        const doDelete = async () => {
-            await apiFetch(`/todos`, "DELETE", { id: id });
+// const initTodo = () => {
+//     // INSERT handleTodoDelete HERE
+//     const handleTodoDelete = (ev, id) => {
+//         const doDelete = async () => {
+//             await apiFetch(`/todos`, "DELETE", { id: id });
     
-            refreshList();
-        }
+//             refreshList();
+//         }
     
-        doDelete().catch(err => console.log("Error changing todo done state", err));
-    };
-    window.handleTodoDelete = handleTodoDelete;
+//         doDelete().catch(err => console.log("Error changing todo done state", err));
+//     };
+//     window.handleTodoDelete = handleTodoDelete;
 
-    const renderTitle = ({ title, done }) => {
-        if (!done) return title;
-        return `<s>${title}</s>`;
-    };
+//     const renderTitle = ({ title, done }) => {
+//         if (!done) return title;
+//         return `<s>${title}</s>`;
+//     };
     
-    const renderItem = ({ id, title, done }) => `
-        <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
-            <input
-                class="form-check-input me-2"
-                type="checkbox"
-                value=""
-                ${done ? "checked" : ""}
-                onchange="handleTodoChange(event, '${id}')"
-            />
-            <button class="delete-button" onclick="handleTodoDelete(event, '${id}')"><i class="fa-solid fa-trash-can"></i></button>
-            <div style="flex-grow: 1;">
-                ${renderTitle({ title, done })}
-            </div>
-            <!-- INSERT DELETE BUTTON HERE -->
-        </li>
-    `;
-    const noItems = `
-        <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
-            <strong>No tasks yet. Try adding one.</strong>
-        </li>
-    `;
+//     const renderItem = ({ id, title, done }) => `
+//         <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
+//             <input
+//                 class="form-check-input me-2"
+//                 type="checkbox"
+//                 value=""
+//                 ${done ? "checked" : ""}
+//                 onchange="handleTodoChange(event, '${id}')"
+//             />
+//             <button class="delete-button" onclick="handleTodoDelete(event, '${id}')"><i class="fa-solid fa-trash-can"></i></button>
+//             <div style="flex-grow: 1;">
+//                 ${renderTitle({ title, done })}
+//             </div>
+//             <!-- INSERT DELETE BUTTON HERE -->
+//         </li>
+//     `;
+//     const noItems = `
+//         <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
+//             <strong>No tasks yet. Try adding one.</strong>
+//         </li>
+//     `;
 
-    const refreshList = () => {
-        const doRefresh = async () => {
-            const list = document.querySelector("#todo-list");
+//     const refreshList = () => {
+//         const doRefresh = async () => {
+//             const list = document.querySelector("#todo-list");
 
-            const resp = await apiFetch("/todos");
-            const todos = await resp.json();
-            if (todos.length === 0) {
-                list.innerHTML = noItems;
-            } else {
-                list.innerHTML = todos.map(renderItem).join("");
-            }
-        };
+//             const resp = await apiFetch("/todos");
+//             const todos = await resp.json();
+//             if (todos.length === 0) {
+//                 list.innerHTML = noItems;
+//             } else {
+//                 list.innerHTML = todos.map(renderItem).join("");
+//             }
+//         };
 
-        doRefresh().catch(err => console.log("Error refreshing list", err));
-    };
+//         doRefresh().catch(err => console.log("Error refreshing list", err));
+//     };
 
-    const addItem = async () => {
-        const input = document.querySelector("#add-input");
-        const title = input.value;
-        if (!title) return;
+//     const addItem = async () => {
+//         const input = document.querySelector("#add-input");
+//         const title = input.value;
+//         if (!title) return;
 
-        await apiFetch("/todos", "POST", { title, done: false });
+//         await apiFetch("/todos", "POST", { title, done: false });
 
-        input.value = "";
-        refreshList();
-    };
+//         input.value = "";
+//         refreshList();
+//     };
 
-    const handleTodoChange = (ev, id) => {
-        const doChange = async () => {
-            await apiFetch(`/todos/${id}`, "PATCH", { done: ev.target.checked });
+//     const handleTodoChange = (ev, id) => {
+//         const doChange = async () => {
+//             await apiFetch(`/todos/${id}`, "PATCH", { done: ev.target.checked });
 
-            refreshList();
-        }
+//             refreshList();
+//         }
 
-        doChange().catch(err => console.log("Error changing todo done state", err));
-    };
-    window.handleTodoChange = handleTodoChange;
+//         doChange().catch(err => console.log("Error changing todo done state", err));
+//     };
+//     window.handleTodoChange = handleTodoChange;
 
-    const form = document.querySelector("#todo-form");
-    form.onsubmit = (ev) => {
-        ev.preventDefault();
-        addItem().catch(err => console.log("Error adding item", err));
-    };
+//     const form = document.querySelector("#todo-form");
+//     form.onsubmit = (ev) => {
+//         ev.preventDefault();
+//         addItem().catch(err => console.log("Error adding item", err));
+//     };
 
-    refreshList();
-}
+//     refreshList();
+// }
 
 
-initTodo();
+// initTodo();
 
 
 const lb = document.getElementById('login-button');
