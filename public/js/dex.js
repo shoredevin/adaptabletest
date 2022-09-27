952ab15e0ebd8bc1b0160670a644f56e2bb875f9
shoredevin <shore.devin@gmail.com>
2022-09-26 20:02:33 -0400

    type

--- public/js/dex.js
+++ public/js/dex.js
@@ -242,7 +242,7 @@ function myFunction() {
 
 async function getPokemonDetails(id, name) {
     console.log(id, name);
-    await fetch(`/todo/details/?name=${name}`)
+    await fetch(`/todos/details/?name=${name}`)
     .then((response) => response.json())
     .then((data) => {
        console.log(data)
