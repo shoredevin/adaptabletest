335dfff599fb6cdefd377ef4ed559247af459e84
shoredevin <shore.devin@gmail.com>
2022-09-30 12:36:24 -0400

    fixing snackbar

--- public/js/dex.js
+++ public/js/dex.js
@@ -280,6 +280,7 @@ function showSnackBar(msg) {
     // Add the "show" class to DIV
     x.className = "show";
     // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 
 const card              = document.querySelector("#yo");
