e3d2380e18855436289db9db2a65f645a8df4e0f
shoredevin <36136055+shoredevin@users.noreply.github.com>
2022-10-02 14:56:15 -0400

    refactor

--- public/js/dex.js
+++ public/js/dex.js
@@ -1,11 +1,46 @@
-window.onload = yeetGet;
-// document.getElementById('sub').onclick = yeet;
-// document.getElementById('get').onclick = yeetGet;
+const card              = document.querySelector("#yo");
+const cardInner         = document.querySelector("#yoyo");
+const front             = document.getElementById('front');
+const back              = document.getElementById('back');
+const pokemonSprite     = document.getElementById('pokemon-sprite')
+const genderIcon        = document.getElementById('gender-icon');
+const shinyIcon         = document.getElementById('shiny-icon');
+const type1Icon         = document.getElementById('type1');
+const type2Icon         = document.getElementById('type2');
+const typeContainer     = document.getElementById("type-container");
+const nextButton        = document.getElementById("next");
+const prevButton        = document.getElementById("prev");
+const dexDetails        = document.getElementById("dex-details");
+const overlay           = document.getElementById("overlay");
+const typeColors = 
+{
+    "fire"      : "#ff4422",
+    "normal"    : "#aaaa99",
+    "water"     : "#3399ff",
+    "grass"     : "#77cc55",
+    "electric"  : "#ffcc33",
+    "ice"       : "#66ccff",
+    "fighting"  : "#bb5544",
+    "poison"    : "#aa5599",
+    "ground"    : "#ddbb55",
+    "flying"    : "#8899ff",
+    "psychic"   : "#ff5599",
+    "bug"       : "#aabb22",
+    "rock"      : "#bbaa66",
+    "ghost"     : "#6666bb",
+    "dark"      : "#775544",
+    "dragon"    : "#7766ee",
+    "steel"     : "#aaaabb",
+    "fairy"     : "#ee99ee"
+};
 
-// <td>
-//     <i class="fa-regular fa-pen-to-square edit-button" onclick="handleEdit(this)"></i>
-//     <i class="fa-regular fa-trash-can delete-button" onclick="handleTodoDelete(event, '${id}')"></i>
-// </td>
+let currentlyMale = true;
+let currentlyShiny = false;
+let shinyIconHasEvent = false;
+let genderIconHasEvent = false;
+
+let data;
+
 const json2table = ({ id, dexnum, name, type1, type2, caught, shiny }) => `
     <tr>
         <td contenteditable="false">${ pad(dexnum, 3) }</td>
@@ -22,12 +57,14 @@ function pad (str, max) {
     return str.length < max ? pad("0" + str, max) : str
 }
 
-async function yeetGet() {
+// window.onload = initializeTable;
+// async function initializeTable() {
+window.onload = async () => {
     await fetch('/todos/dex')
      .then((response) => response.json())
      .then((data) => {
         console.log(data)
-        let myTable = document.querySelector("#myTable");
+        // let myTable = document.querySelector("#myTable");
         // myTable.style.display = "inline-block";
         let tbdy = document.getElementById('myTable').getElementsByTagName('tbody')[0];
         tbdy.innerHTML = data.map(json2table).join("")
@@ -35,151 +72,107 @@ async function yeetGet() {
     });
     setTotalRows();
 }
-async function yeet() {
-    const dexnum = document.getElementById('dexnum').value;
-    const name = document.getElementById('name').value;
-    const type1 = document.getElementById('type1-selector').value;
-    const type2 = document.getElementById('type2-selector').value;
 
-    if(isNaN(dexnum)) { console.log("NaN"); return }
-    if(!dexnum || !name || !type1 || !type2) return
-    await fetch('/todos/dex', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ 
-            dexnum: dexnum, 
-            name: name,
-            type1: type1,
-            type2: type2,
-            caught: false,
-            shiny: false
-        })
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        document.getElementById('dexnum').value = "";
-        document.getElementById('name').value = "";
-        yeetGet();
-    })
-}
+// async function yeet() {
+//     const dexnum = document.getElementById('dexnum').value;
+//     const name = document.getElementById('name').value;
+//     const type1 = document.getElementById('type1-selector').value;
+//     const type2 = document.getElementById('type2-selector').value;
 
-async function handleTodoDelete(ev, id) {
-    if(!window.confirm("Are you sure you want to delete?")) return;
-    const resp = await fetch(`/todos/dex`, {
-        method: "DELETE", 
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ id: id })
-    });
-    const todos = await resp.json();
-    console.log(todos);
-    await yeetGet();
-};
-
-async function handleEdit(e) {
-    console.log(e.parentElement.parentElement);
-    const rows = e.parentElement.parentElement.children;
-    console.log(rows.length);
-    for(let i = 1; i < rows.length; i++) {
-        const bool = (rows[i].contentEditable === 'true');
-        if(bool) {
-            rows[i].contentEditable = false;
-            e.style.color = "black";
-        }
-        if(!bool) {
-            rows[i].contentEditable = true;
-            e.style.color = "orange";
-        }
-    }   
-
-}
-
-async function patchJob(e, id, bool) {
-    // console.log(e);
-    if(e.target.classList.contains("caught-button")) {
-        if(e.target.parentElement.nextElementSibling.children[0].classList.contains("fa-solid")) {
-            showSnackBar("Cannot make a shiny Pokemon uncaught");
-            return;
-        };
-        // e.target.classList.toggle("caught-button");
-        e.target.classList.toggle("fa-regular");
-        e.target.classList.toggle("fa-solid");
-    };
-    if(e.target.classList.contains("shiny-button")) {
-        if(e.target.parentElement.previousElementSibling.children[0].classList.contains("fa-regular")) {
-            showSnackBar("Cannot make an uncaught Pokemon shiny");
-            return;
-        };
-        e.target.classList.toggle("fa-regular");
-        e.target.classList.toggle("fa-solid");
-        // e.target.classList.toggle("shiny-button");
-        // e.target.classList.toggle("caught-button");
-    };
-    const resp = await fetch(`/todos/dex/${id}`, {
-        method: "PATCH", 
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(bool)
-    });
-    const todos = await resp.json();
-    console.log(todos)
-    // await yeetGet();
-}
-
-// async function json2table(data) {
-//     let myTable = document.querySelector("#myTable");
-//     myTable.style.display = "inline-block";
-//     let tbdy = document.getElementById('myTable').getElementsByTagName('tbody')[0];
-//     tbdy.innerHTML = "";
-//     // let rows = data.response;
-//     let rows = data;
-//     // console.log(rows.length);
-//     const numCols = Object.keys(rows[1]).length;
-//     for (let i = 0; i < rows.length; i++) {
-//         let tr = document.createElement('tr');
-//         for(let k = 0; k < numCols; k++) {
-//             const val = Object.values(rows[i])[k];
-//             tr.appendChild(document.createElement('td'));
-//             if(k == 0) { 
-//                 tr.cells[k].appendChild(document.createElement("button"))
-//                 button.innerHTML = `<button class="edit-button" title="Edit" onclick="handleEdit(this)"><i class="fa-regular fa-pen-to-square"></i></button>`; 
-//             } else {
-//                 tr.cells[k].appendChild(document.createTextNode(val));
-//             }
-//         }
-//         tbdy.appendChild(tr);
-//     }
+//     if(isNaN(dexnum)) { console.log("NaN"); return }
+//     if(!dexnum || !name || !type1 || !type2) return
+//     await fetch('/todos/dex', {
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ 
+//             dexnum: dexnum, 
+//             name: name,
+//             type1: type1,
+//             type2: type2,
+//             caught: false,
+//             shiny: false
+//         })
+//     })
+//     .then((response) => response.json())
+//     .then((data) => {
+//         console.log(data);
+//         document.getElementById('dexnum').value = "";
+//         document.getElementById('name').value = "";
+//         yeetGet();
+//     })
 // }
 
-// function myFunction() {
-//   var input, filter, table, tr, td, i, txtValue;
-//   input = document.getElementById("myInput");
-//   filter = input.value.toUpperCase();
-//   table = document.getElementById("myTable");
-//   tr = table.getElementsByTagName("tr");
-//   for (i = 0; i < tr.length; i++) {
-//     td = tr[i].getElementsByTagName("td")[0];
-//     if (td) {
-//       txtValue = td.textContent || td.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         tr[i].style.display = "";
-//       } else {
-//         tr[i].style.display = "none";
-//       }
-//     }       
-//   }
+// async function handleTodoDelete(ev, id) {
+//     if(!window.confirm("Are you sure you want to delete?")) return;
+//     const resp = await fetch(`/todos/dex`, {
+//         method: "DELETE", 
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ id: id })
+//     });
+//     const todos = await resp.json();
+//     console.log(todos);
+//     await yeetGet();
+// };
+
+// async function handleEdit(e) {
+//     console.log(e.parentElement.parentElement);
+//     const rows = e.parentElement.parentElement.children;
+//     console.log(rows.length);
+//     for(let i = 1; i < rows.length; i++) {
+//         const bool = (rows[i].contentEditable === 'true');
+//         if(bool) {
+//             rows[i].contentEditable = false;
+//             e.style.color = "black";
+//         }
+//         if(!bool) {
+//             rows[i].contentEditable = true;
+//             e.style.color = "orange";
+//         }
+//     }   
+
+// }
+
+// async function patchJob(e, id, bool) {
+//     // console.log(e);
+//     if(e.target.classList.contains("caught-button")) {
+//         if(e.target.parentElement.nextElementSibling.children[0].classList.contains("fa-solid")) {
+//             showSnackBar("Cannot make a shiny Pokemon uncaught");
+//             return;
+//         };
+//         // e.target.classList.toggle("caught-button");
+//         e.target.classList.toggle("fa-regular");
+//         e.target.classList.toggle("fa-solid");
+//     };
+//     if(e.target.classList.contains("shiny-button")) {
+//         if(e.target.parentElement.previousElementSibling.children[0].classList.contains("fa-regular")) {
+//             showSnackBar("Cannot make an uncaught Pokemon shiny");
+//             return;
+//         };
+//         e.target.classList.toggle("fa-regular");
+//         e.target.classList.toggle("fa-solid");
+//         // e.target.classList.toggle("shiny-button");
+//         // e.target.classList.toggle("caught-button");
+//     };
+//     const resp = await fetch(`/todos/dex/${id}`, {
+//         method: "PATCH", 
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(bool)
+//     });
+//     const todos = await resp.json();
+//     console.log(todos)
+//     // await yeetGet();
 // }
 
 function setSearchLogic() {
     console.log('i am here...')
         const regex = / & /gi;
 		let index;
-		
         var value = document.querySelector('#myInput').value.toLowerCase().replace(regex, '&');
         const caughtRegX = /caught/gi;
         const uncaughtRegX = /!caught/gi;
@@ -291,53 +284,6 @@ function showSnackBar(msg) {
     // After 3 seconds, remove the show class from DIV
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
-
-const card              = document.querySelector("#yo");
-const cardInner         = document.querySelector("#yoyo");
-const front             = document.getElementById('front');
-const back              = document.getElementById('back');
-const pokemonSprite     = document.getElementById('pokemon-sprite')
-const genderIcon        = document.getElementById('gender-icon');
-const shinyIcon         = document.getElementById('shiny-icon');
-const type1Icon         = document.getElementById('type1');
-const type2Icon         = document.getElementById('type2');
-const typeContainer     = document.getElementById("type-container");
-const nextButton        = document.getElementById("next");
-const prevButton        = document.getElementById("prev");
-const dexDetails        = document.getElementById("dex-details");
-const overlay           = document.getElementById("overlay");
-
-// const formSelector      = document.getElementById("form-selector");
-
-let currentlyMale = true;
-let currentlyShiny = false;
-
-let shinyIconHasEvent = false;
-let genderIconHasEvent = false;
-
-let data;
-
-const typeColors = 
-{
-    "fire"      : "#ff4422",
-    "normal"    : "#aaaa99",
-    "water"     : "#3399ff",
-    "grass"     : "#77cc55",
-    "electric"  : "#ffcc33",
-    "ice"       : "#66ccff",
-    "fighting"  : "#bb5544",
-    "poison"    : "#aa5599",
-    "ground"    : "#ddbb55",
-    "flying"    : "#8899ff",
-    "psychic"   : "#ff5599",
-    "bug"       : "#aabb22",
-    "rock"      : "#bbaa66",
-    "ghost"     : "#6666bb",
-    "dark"      : "#775544",
-    "dragon"    : "#7766ee",
-    "steel"     : "#aaaabb",
-    "fairy"     : "#ee99ee"
-};
 
 function openCard(name, type1, type2) {
     overlay.classList.toggle("active");
@@ -496,86 +442,6 @@ function setFormSelector() {
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
-
-// const pokemonData = {
-//     'bulbasaur': {
-//         "nickname": "Seed Pokemon",
-//         "ability1": "Overgrow",
-//         "ability2": "",
-//         "hability": "Chlorophyll",
-//         "eggGroup": "Monster and Grass",
-//         "catchRate": "11.9%",
-//         "hatchRate": "5140 - 5396 steps",
-//         "levelingRate": "Medium Slow",
-//         "mPercent": "87.5%",
-//         "fPercent": "12.5%",
-//         "spriteURL": "https://poketools.info/images/sprites/001.gif",
-//         "spriteURLShiny": "https://poketools.info/images/sprites/001-s.gif"
-//     },
-//     'pikachu': {
-//         "nickname": "Mouse Pokemon",
-//         "ability1": "Static",
-//         "ability2": "",
-//         "hability": "Lightning Rod",
-//         "eggGroup": "Field and Fairy",
-//         "catchRate": "35.2%",
-//         "hatchRate": "2570 - 2826 steps",
-//         "levelingRate": "Medium Fast",
-//         "mPercent": "50%",
-//         "fPercent": "50%",
-//         "spriteURL": "https://poketools.info/images/sprites/025.gif",
-//         "spriteURLShiny": "https://poketools.info/images/sprites/025-s.gif"
-//     },
-//     'burmy': {
-//         "nickname": "Bagworm Pokemon",
-//         "ability1": "Shed Skin",
-//         "ability2": "",
-//         "hability": "Overcoat",
-//         "eggGroup": "Bug",
-//         "catchRate": "24.9",
-//         "hatchRate": "3855 - 4111 steps",
-//         "levelingRate": "Medium Fast",
-//         "mPercent": "50%",
-//         "fPercent": "50%",
-//         "spriteURL": "https://poketools.info/images/sprites/412.gif",
-//         "spriteURLShiny": "https://poketools.info/images/sprites/412-s.gif",
-//         "femaleSpriteURL": "",
-//         "femaleSpriteURLShiny": "",
-//         "forms": {
-//             "Plant Cloak": {
-//                 "url": "https://poketools.info/images/sprites/412.gif", 
-//                 "type1": "Bug",
-//                 "type2": ""
-//             },
-//             "Sandy Cloak": {
-//                 "url": "https://poketools.info/images/sprites/412sc.gif", 
-//                 "type1": "Bug",
-//                 "type2": ""
-//             },
-//             "Trash Cloak": {
-//                 "url": "https://poketools.info/images/sprites/412tc.gif", 
-//                 "type1": "Bug",
-//                 "type2": ""
-//             }
-//         }
-//     },
-//     'combee': {
-//         "nickname": "Tiny Bee Pokemon",
-//         "ability1": "Honey Gather",
-//         "ability2": "",
-//         "hability": "Hustle",
-//         "eggGroup": "Bug",
-//         "catchRate": "24.9",
-//         "hatchRate": "3855 - 4111 steps",
-//         "levelingRate": "Medium Slow",
-//         "mPercent": "87.5%",
-//         "fPercent": "12.5%",
-//         "spriteURL": "https://poketools.info/images/sprites/415.gif",
-//         "spriteURLShiny": "https://poketools.info/images/sprites/415-s.gif",
-//         "femaleSpriteURL": "https://poketools.info/images/sprites/415f.gif",
-//         "femaleSpriteURLShiny": "https://poketools.info/images/sprites/415f-s.gif"
-//     }
-// }
 
 function clearSearch() {
     document.getElementById('myInput').value = '';
