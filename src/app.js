51acf89b6fa4724f252fa0b3a7a63ac2068475bd
shoredevin <shore.devin@gmail.com>
2022-10-06 21:05:43 -0400

    does this fix mime issues?

--- src/app.js
+++ src/app.js
@@ -11,16 +11,20 @@ const app = express();
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Routes
 const todosRouter = require('./routes/todos');
 app.use('/todos', todosRouter);
 
-
+/*
+    Add routing for login path here...
+*/
 
 app.get('/dex', async (req, res) => {
+    //if authenticated send here
     res.sendFile(path.join(__dirname, '../public/dex.html'));
+    //else send to login page
 })
   
 
