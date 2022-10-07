873b9c1db2bb46dd7057a9b46b27dcd5f0aa57ef
shoredevin <shore.devin@gmail.com>
2022-10-06 23:40:38 -0400

    one last try

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -75,7 +75,7 @@ router.post("/login", (req, res) => {
   const sessionId = "1234";
   res.cookie("session", sessionId, {  maxAge: 900000, httpOnly: true });
   // 'Set-Cookie', `session=${sessionId}`)
-  return res.sendFile(path.join(__dirname, '../../public/dex.html'));
+  return res.sendFile(path.join(__dirname, '../public/dex.html'));
   // res.send({ res: 'success' });
 });
 
