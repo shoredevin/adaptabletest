503aba9d83a9772e34349073772e2a2ad50afd24
shoredevin <shore.devin@gmail.com>
2022-10-06 21:44:29 -0400

    req.body trouble

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -62,7 +62,10 @@ router.post("/", asyncMiddleware(async (req, res) => {
 
 //auth route
 router.post("/login", (req, res) => {
-  const { username, password } = req.body;
+  console.log(req.body);
+  const username = req.body.username;
+  const password = req.body.password;
+  // const { username, password } = req.body;
   if(username != "shore.devin" || password != "password") {
     return res.status('401').send({ res: 'Invalid username or password' })
   }
