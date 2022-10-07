90e925275a544e82ab5c6f8a75444c49d261d091
shoredevin <shore.devin@gmail.com>
2022-10-06 20:51:48 -0400

    does this break everything?

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+// const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
 
-const todosRouter = require('./routes/todos');
+// const todosRouter = require('./routes/todos');
 
-const app = express();
+// const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Routes
 app.use('/todos', todosRouter);
