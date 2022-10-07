abd0d1190207e6a8ebcc61c98948c15bb25670af
shoredevin <shore.devin@gmail.com>
2022-10-06 22:56:20 -0400

    cookies

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 // const logger = require('morgan');
 
 
@@ -10,7 +10,7 @@ const app = express();
 // app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Routes
