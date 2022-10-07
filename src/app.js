a87a5a192cdf08ba4e1b52218a24aed5ba3efb27
shoredevin <shore.devin@gmail.com>
2022-10-07 08:18:02 -0400

    remove public

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Routes
 const todosRouter = require('./routes/todos');
