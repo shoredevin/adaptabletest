64c9057495ad7eebfde2f9337d5dabacdd74a382
shoredevin <36136055+shoredevin@users.noreply.github.com>
2022-10-08 10:07:50 -0400

    home

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 const myLogger = function (req, res, next) {
     console.log(req.path)
     if(req.path.includes('.html')) {
-        res.send('/');
+        res.send(__dirname, '../public/index.html');
     }
     console.log('LOGGED');
     next();
