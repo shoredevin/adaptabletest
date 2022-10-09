c381858d02130e8cfcde48c7a57753010b8c2c30
shoredevin <36136055+shoredevin@users.noreply.github.com>
2022-10-09 11:46:46 -0400

    404 page

--- src/app.js
+++ src/app.js
@@ -27,44 +27,22 @@ app.use(myLogger)
 const authCheck = async function(req, res, next) {
     const userCookie = req.cookies.app_user;
     const sessionCookie = req.cookies.app_session;
-
-    /* if cookie does not exist return not autherized */
     if (userCookie == undefined || sessionCookie == undefined) { 
-        // authenticatedState = false;
         res.clearCookie('app_user');
         res.clearCookie('app_session');
         res.locals.authenticated = false;
         next();
         return;
     }
-
-    /* get user/session from server */
     const userDetails = await prisma.Users.findUnique({
         where: { username: userCookie }
     });
-    
-    /* compare user/session (cookie:server) */
-    // if (userDetails === null || userCookie != userDetails.username || sessionCookie != userDetails.sessionId) {
-    //     authenticatedState = false;
-    //     res.locals.authenticated = authenticatedState;
-    //     next();
-    // }
-    /* if comparison checks out auth = true */
-
-    /* if comparison does not check out auth = false and clear cookie */
-    
-    /* Compare cookie information with server information */
     const authenticatedState = 
         req.cookies.app_user && 
         req.cookies.app_session && 
         userCookie == userDetails.username && 
         sessionCookie == userDetails.sessionId 
         ? true : false;
-    
-    /* 
-        - To Do -
-        if authenticatedState is false clear cookies 
-    */ 
     if (authenticatedState == false) { 
         res.clearCookie('app_user');
         res.clearCookie('app_session');
@@ -73,23 +51,12 @@ const authCheck = async function(req, res, next) {
     res.locals.authenticated = authenticatedState;
     next();
 }
-    // app.use(authCheck);
-
-// app.get('/dex.html', async (req, res) => {
-//     res.send({ res: "not found" });
-// })
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
 const todosRouter = require('./routes/todos');
 app.use('/todos', todosRouter);
-
-/*
-    Add routing for login path here...
-*/
-
-// const sessions = {}
 
 app.get('/', authCheck, async (req, res) => {
     console.log('/ authenticated state checker: ', res.locals.authenticated)
@@ -106,16 +73,15 @@ app.get('/cool', authCheck, async (req, res) => {
 
 app.get('/dex', authCheck, async (req, res) => {
     console.log('/ authenticated state checker: ', res.locals.authenticated)
-    // const sessionId = req.headers.cookie?.split('=')[1];
-    // const cookies = req.cookies;
-    // console.log(cookies);
-    // console.log(req.cookies.app_user ? "true" : "false");
-    // console.log(req.cookies.app_session ? "true" : "false"); 
     if(!res.locals.authenticated) {
         return res.redirect('/');;
     }
     res.sendFile(path.join(__dirname, '../public/dex.html'));
 })
-  
+
+//404 route
+app.use((req, res, next) => {
+    res.sendfile(path.join(__dirname, '../public/dex.html'))
+});
 
 module.exports = app;
