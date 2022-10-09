5b00938584b60377dbcd11cf06e5f7a72bb396f1
shoredevin <36136055+shoredevin@users.noreply.github.com>
2022-10-09 09:07:37 -0400

    on the right track

--- src/app.js
+++ src/app.js
@@ -66,7 +66,7 @@ app.get('/dex', authCheck, async (req, res) => {
     // console.log(req.cookies.app_user ? "true" : "false");
     // console.log(req.cookies.app_session ? "true" : "false"); 
     if(!res.locals.authenticated) {
-        return res.redirect('/login.html');;
+        return res.redirect('/');;
     }
     res.sendFile(path.join(__dirname, '../public/dex.html'));
 })
