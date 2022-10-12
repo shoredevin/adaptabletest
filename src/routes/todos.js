458130774a975c0b8faa25ef99096e209ded9b1a
shoredevin <36136055+shoredevin@users.noreply.github.com>
2022-10-11 21:03:51 -0400

    typo

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -172,8 +172,12 @@ router.post("/dex", authenticationMiddleware, asyncMiddleware(async (req, res) =
   //   {"dexnum": -1, "name": "jessmon", "type1": "flying", "type2": "fairy"}
   // ]
   const usersToUpdate = await prisma.Users.findMany({
-    where: { hasPokedex: !false },
-    select: { username }
+    where: { 
+      hasPokedex: !false 
+    },
+    select: { 
+      username: true
+    }
   });
   res.json({ usersToUpdate });
   // BELOW NEEDS TO BE ADDED BACK IN
