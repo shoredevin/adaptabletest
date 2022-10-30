04972571e499b536c8d0b4f5755bf1ebd00a9e76
shoredevin <36136055+shoredevin@users.noreply.github.com>
2022-10-30 16:51:03 -0400

    typo

--- src/routes/api.js
+++ src/routes/api.js
@@ -274,7 +274,7 @@ router.get("/details", asyncMiddleware(async (req, res) => {
 }));
 
 router.get("/details/all", asyncMiddleware(async (req, res) => {
-  const details = await prisma.Pokemon.findMany({});
+  const details = await prisma.Pokedex.findMany({});
   res.json(details);
 }));
 
